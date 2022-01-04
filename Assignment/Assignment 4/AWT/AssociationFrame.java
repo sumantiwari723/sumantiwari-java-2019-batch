1ed4aa695edf0b57ed192f2a272607efd5d2b79c
sumantiwari723 <81008569+sumantiwari723@users.noreply.github.com>
2022-01-04 20:43:29 +0545

    Create AssociationFrame.java

---
+++ Assignment/Assignment 4/AWT/AssociationFrame.java
@@ -0,0 +1,50 @@
+import java.awt.Frame;
+import java.awt.Button;
+import java.awt.Label;
+import java.awt.TextField;
+class AssociationFrame
+{
+	public static void main(String[] args)
+	{
+		Frame f=new Frame();
+		Label l1=new Label("First Number");
+		Label l2=new Label("Second Number");
+		Label l3=new Label("Result");
+		TextField t1=new TextField();
+		TextField t2=new TextField();
+		TextField t3=new TextField();
+		Button b1=new Button("Additon");
+		Button b2=new Button("Subtraction");
+		Button b3=new Button("Multiply");
+		Button b4=new Button("Divide");
+
+		l1.setBounds(50,100,100,20);
+		l2.setBounds(50,140,100,20);
+		l3.setBounds(50,180,100,20);
+		t1.setBounds(200,100,100,20);
+		t2.setBounds(200,140,100,20);
+		t3.setBounds(200,180,100,20);
+		b1.setBounds(50,250,50,20);
+		b2.setBounds(110,250,50,20);
+		b3.setBounds(170,250,50,20);
+		b4.setBounds(230,250,50,20);
+
+		f.add(l1);
+		f.add(l2);
+		f.add(l3);
+		f.add(t1);
+		f.add(t2);
+		f.add(t3);
+		f.add(b1);
+		f.add(b2);
+		f.add(b3);
+		f.add(b4);
+
+
+		f.setLayout(null);
+		f.setVisible(true);
+		f.setSize(400,350);
+
+	}
+
+}
