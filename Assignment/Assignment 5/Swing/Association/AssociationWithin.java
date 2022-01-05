2b09c2a9ceb0225b4a8b697412cbecfb5dba76ca
sumantiwari723 <81008569+sumantiwari723@users.noreply.github.com>
2022-01-05 22:57:03 +0545

    Create AssociationWithin.java

---
+++ Assignment/Assignment 5/Swing/Association/AssociationWithin.java
@@ -0,0 +1,82 @@
+import javax.swing.*;
+import java.awt.event.*;
+class AssociationWithin
+{
+	public static void main(String[] args)
+	{
+		JFrame f=new JFrame();
+		JLabel l1=new JLabel("First Number");
+		JLabel l2=new JLabel("Second Number");
+		JLabel l3=new JLabel("Result");
+		JTextField t1=new JTextField();
+		JTextField t2=new JTextField();
+		JTextField t3=new JTextField();
+		JButton b1=new JButton("Additon");
+		JButton b2=new JButton("Subtraction");
+		JButton b3=new JButton("Multiply");
+		JButton b4=new JButton("Divide");
+
+		l1.setBounds(50,100,100,20);
+		l2.setBounds(50,140,100,20);
+		l3.setBounds(50,180,100,20);
+		t1.setBounds(200,100,100,20);
+		t2.setBounds(200,140,100,20);
+		t3.setBounds(200,180,100,20);
+		b1.setBounds(50,250,50,20);
+		b2.setBounds(110,250,50,20);a
+		b3.setBounds(170,250,50,20);
+		b4.setBounds(230,250,50,20);
+
+		f.add(l1);
+		f.add(l2);
+		f.add(l3);
+		f.add(t1);
+		f.add(t2);
+		f.add(t3);
+		f.add(b1);
+		f.add(b2);
+		f.add(b3);
+		f.add(b4);
+
+		b1.addActionListener(this);
+		b2.addActionListener(this);
+		b3.addActionListener(this);
+		b4.addActionListener(this);
+
+		b1.setBorder(null);
+		b2.setBorder(null);
+		b3.setBorder(null);
+		b4.setBorder(null);
+
+	}
+public void actionPerformed(ActionEvent event)
+	{
+		int n1=Integer.parseInt(t1.getText());
+		int n2=Integer.parseInt(t2.getText());
+		try{
+
+		if(event.getSource()==b1)
+		{
+			t3.setText(String.valueOf(n1+n2));
+		}
+		if(event.getSource()==b2)
+		{
+			t3.setText(String.valueOf(n1-n2));
+		}
+		if(event.getSource()==b3)
+		{
+			t3.setText(String.valueOf(n1*n2));
+		}
+			if(event.getSource()==b4)
+		{
+			t3.setText(String.valueOf(n1/n2));
+		}
+		 } catch (Exception e) {
+            t3.setText("Invalid"+" "+e.getMessage());
+        }
+	}
+		public static void main(String[] args)
+	{
+		new AssociationWithin();
+	}
+}
