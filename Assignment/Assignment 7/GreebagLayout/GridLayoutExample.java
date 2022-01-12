b31ff8b37a55fe615d014dde2b4f9d1a0a244aa1
sumantiwari723 <81008569+sumantiwari723@users.noreply.github.com>
2022-01-12 22:38:46 +0545

    Create GridLayoutExample.java

---
+++ Assignment/Assignment 7/GreebagLayout/GridLayoutExample.java
@@ -0,0 +1,43 @@
+import java.awt.*;    
+import javax.swing.*;    
+    
+public class GridLayoutExample  
+{    
+JFrame f;    
+  
+// constructor  
+GridLayoutExample()  
+{    
+JFrame f = new JFrame();    
+  
+// creating 9 buttons  
+JButton btn1 = new JButton("1");    
+JButton btn2 = new JButton("2");    
+JButton btn3 = new JButton("3");    
+JButton btn4 = new JButton("4");    
+JButton btn5 = new JButton("5");    
+JButton btn6 = new JButton("6");    
+JButton btn7 = new JButton("7");    
+JButton btn8 = new JButton("8");    
+JButton btn9 = new JButton("9");    
+    
+// adding buttons to the frame  
+ 
+f.add(btn1); f.add(btn2); f.add(btn3);  
+f.add(btn4); f.add(btn5); f.add(btn6);  
+f.add(btn7); f.add(btn8); f.add(btn9);    
+  
+// setting the grid layout using the parameterless constructor    
+f.setLayout(new GridLayout());    
+  
+  
+f.setSize(300, 300);    
+f.setVisible(true);    
+}  
+  
+// main method  
+public static void main(String args[])   
+{    
+new GridLayoutExample();    
+}    
+}    
