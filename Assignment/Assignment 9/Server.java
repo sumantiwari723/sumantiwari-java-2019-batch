9294e5300d65f9c927f5f977f9c39d0e873f9211
sumantiwari723 <81008569+sumantiwari723@users.noreply.github.com>
2022-01-22 17:30:10 +0545

    Create Server.java

---
+++ Assignment/Assignment 9/Server.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private ServerSocket serverSocket;
+    public Server(ServerSocket serverSocket){
+        this.serverSocket = serverSocket;
+    }
+    public void startServer(){
+        try {
+            while(!serverSocket.isClosed()){
+                Socket socket = serverSocket.accept();
+                System.out.println("A New Client Has Connected");
+                ClientHandle clientHandler = new ClientHandle(socket);
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    public void closeServerSocket(){
+        try{
+            if (serverSocket!=null){
+                serverSocket.close();
+            }
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(1234);
+        Server server = new Server(serverSocket);
+        server.startServer();
+    }
+}
